6482b226656815455e34a58cf32062aa537241a2
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-05-26 23:21:15 -0600

    Add JWT authentication and user details service
    Introduce `JwtAuthenticationFilter` for processing JWT-based authentication and `UserDetailService` for loading user details from the database. These components enable secure user authentication and integration with Spring Security.

---
+++ src/main/java/com/healthcare/appointmentsystem/security/UserDetailService.java
@@ -0,0 +1,33 @@
+package com.healthcare.appointmentsystem.security;
+
+import com.healthcare.appointmentsystem.model.User;
+import com.healthcare.appointmentsystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(),
+                user.isAccountNonLocked(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()))
+        );
+    }
+}
